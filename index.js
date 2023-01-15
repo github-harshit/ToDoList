73e043ef1f76818d0dfeaa4f08e7ed397235b7d4
Harshit Joshi <harshitjoshi250@gmail.com>
2023-01-15 21:15:12 +0530

    route created for post request of tasks

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express= require("express"); 
 const port = 8000; 
 const app = express(); 
+ 
+ app.set("view engine", "ejs"); 
+app.set("views", "./views"); 
+ app.use(express.static("assets")); 
+  app.use(express.urlencoded()); 
+  
+
+ app.post("/create_task", function(req, res){
+    console.log(req.body); 
+     return res.redirect("/"); 
+
+ })
+
+app.get("/", function(req, res){
+    return res.render("home", {
+        title: "ToDoList"
+    })
+})
+
 app.listen(port, function(err){
     if(err){
         console.log("error listening to port"); 
         return; 
     }
-    console.log("sucessfully listienin gto port", port); 
-})
+    console.log("sucessfully listening to port", port); 
+}); 
